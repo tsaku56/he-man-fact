bd21dd77b3bfe050d8dd8e7bfe70ffb4a9822248
Ross <blindross@gmail.com>
2023-05-03 11:57:26 +0100

    really

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>He-man nemsis is skeletor</h1>
-      <img src="https://i.imgflip.com/1k3u0b.jpg?a467400" />
+      <h1>He-man's nemsis is skeletor</h1>
+      <img src="https://i.imgflip.com/1k3u0b.jpg?a467400" alt="skeletor" title="skele" />
     </div>
   );
 }
